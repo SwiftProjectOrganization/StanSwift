a7e932e97b44e251d7487a9a4c28e89f0a1d3d72
Rob J Goedman <goedman@icloud.com>
2025-10-07 10:11:52 -0600

    Added Readme, added ..._summary.csv file creation for stansummary

--- StanSwift.playground/Sources/Basics.swift
+++ StanSwift.playground/Sources/Basics.swift
@@ -53,11 +53,20 @@ public func stanRun(modelPath: String,
 public func stanSummary(modelPath: String,
                         model: String,
                         cmdstan: String) -> (String, String) {
-  
-  swiftSyncFileExec(program: cmdstan + "/bin/stansummary",
+  let fileManager = FileManager.default
+  let filePath = modelPath + "/" + model + "_summary.csv"
+
+  do {
+      try fileManager.removeItem(atPath: filePath)
+      print("\(model)_summary.csv deleted successfully, will create a new one.")
+  } catch {
+      print("Error deleting file \(model)_summary.csv: \(error)")
+  }
+  return swiftSyncFileExec(program: cmdstan + "/bin/stansummary",
                     arguments: [ modelPath + "/" + model + "_output_1.csv",
                                 modelPath + "/" + model + "_output_2.csv",
                                 modelPath + "/" + model + "_output_3.csv",
-                                modelPath + "/" + model + "_output_4.csv" ])
+                                modelPath + "/" + model + "_output_4.csv",
+                               "--csv_filename", modelPath + "/" + model + "_summary.csv"])
 }
 
