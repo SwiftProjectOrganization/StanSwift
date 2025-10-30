9322151f983d2bcf55b23eb4e638a0a07b9eeb0b
Rob J Goedman <goedman@icloud.com>
2025-10-30 17:23:32 -0400

    Fixed typo

--- StanSwift.playground/Sources/Pathfinder.swift
+++ StanSwift.playground/Sources/Pathfinder.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public func pathfinder(modelPath: String,
-                    model: String,
-                    arguments: [String] = ["optimize"]) -> (String, String) {
+                       model: String,
+                       arguments: [String] = ["optimize"]) -> (String, String) {
   var args = arguments
   args.append(contentsOf: ["data", "file=" + modelPath + "/" + model + ".data.json"])
   args.append(contentsOf: ["output", "file=" + modelPath + "/" + model + "_pathfinder.csv"])
@@ -19,31 +19,28 @@ public func pathfinder(modelPath: String,
 }
 
 public func getPathfinderResult(result: String) -> [String] {
-                          
+  
   var theResult: [String] = []
   var copy = false
-
+  
   do {
-          do {
-            let myStrings = result.components(separatedBy: .newlines)
-            for result in myStrings {
-              if result.count > 0 {
-                let index = result.index(result.startIndex, offsetBy: 0)
-                let index4 = result.index(result.startIndex, offsetBy: 4)
-                let contents = result[index...index4]
-                if contents != "Path" {
-                  copy = true
-                }
-                if copy {
-                  print(result)
-                  theResult.append(result)
-                }
-              }
-            }
-          } catch {
-            print(error.localizedDescription)
+    do {
+      let myStrings = result.components(separatedBy: .newlines)
+      for result in myStrings {
+        if result.count > 0 {
+          let index = result.index(result.startIndex, offsetBy: 0)
+          let index4 = result.index(result.startIndex, offsetBy: 4)
+          let contents = result[index...index4]
+          if contents != "Path" {
+            copy = true
           }
+          if copy {
+            print(result)
+            theResult.append(result)
+          }
+        }
       }
+    }
   }
   return theResult
 }
